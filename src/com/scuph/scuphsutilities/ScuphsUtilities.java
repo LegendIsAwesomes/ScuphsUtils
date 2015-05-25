8a2ffba1129256d880aa808f37fe9f63194c1af6
Scuph <hentscheljr1@aol.com>
2015-05-24 21:19:33 -0400

    Update ScuphsUtilities.java

--- src/com/scuph/scuphsutilities/ScuphsUtilities.java
+++ src/com/scuph/scuphsutilities/ScuphsUtilities.java
@@ -3,20 +3,12 @@ package com.scuph.scuphsutilities;
 import com.scuph.scuphsutilities.listeners.ScuphsUtilitiesListener;
 import com.scuph.scuphsutilities.commands.Command_owner;
 import com.scuph.scuphsutilities.commands.Command_iwantadmin;
-<<<<<<< HEAD
 import com.scuph.scuphsutilities.commands.Command_developer;
-=======
-import com.scuph.scuphsutilities.commands.Command_developers;
->>>>>>> origin/master
 import com.scuph.scuphsutilities.commands.Command_information;
-import com.scuph.scuphsutilities.commands.Command_manuadd;
 import com.scuph.scuphsutilities.commands.Command_requesthelp;
 import com.scuph.scuphsutilities.commands.Command_welcome;
-<<<<<<< HEAD
 import com.scuph.scuphsutilities.listeners.joinListener;
 import com.scuph.scuphsutilities.listeners.leaveListener;
-=======
->>>>>>> origin/master
 import net.pravian.bukkitlib.BukkitLib;
 import net.pravian.bukkitlib.command.BukkitCommandHandler;
 import net.pravian.bukkitlib.config.YamlConfig;
@@ -51,14 +43,9 @@ public class ScuphsUtilities extends BukkitPlugin {
         handler.setCommandLocation(Command_owner.class.getPackage());
         handler.setCommandLocation(Command_iwantadmin.class.getPackage());
         handler.setCommandLocation(Command_welcome.class.getPackage());
-<<<<<<< HEAD
         handler.setCommandLocation(Command_requesthelp.class.getPackage());
         handler.setCommandLocation(Command_developer.class.getPackage());
-=======
-        handler.setCommandLocation(Command_manuadd.class.getPackage());
         handler.setCommandLocation(Command_requesthelp.class.getPackage());
-        handler.setCommandLocation(Command_developers.class.getPackage());
->>>>>>> origin/master
         
         LoggerUtils.info(plugin, "for " + config.getString("server-name") + " Version: " + plugin.getVersion() + " by " + plugin.getAuthor() + " is enabled.");
   }
